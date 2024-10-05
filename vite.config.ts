f09f8d860a350491f3543e273ecebc20165c4853
LuckyFoxCode <sergei181989@gmail.com>
2024-10-05 18:21:40 +0300

    chore: setup github actions and setup CI.yml

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
+const base = process.env.VITE_BASE_URL || '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
   resolve: {
     alias: {
